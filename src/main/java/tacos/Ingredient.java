0dd9df8fb41f867989416ce840e09c9eca289f05
ksw2003 <kimseowon2003@gmail.com>
2025-06-03 15:48:48 +0900

    Add full project folder

---
+++ src/main/java/tacos/Ingredient.java
@@ -0,0 +1,25 @@
+package tacos;
+
+import lombok.*;
+import jakarta.persistence.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor(access= AccessLevel.PROTECTED, force = true)
+@Document(collection="ingredients")
+public class Ingredient {
+    @Id
+    private String id;
+    private String name;
+    private Type type;
+
+    public static enum Type{
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+    public Type getType() {return type;}
+    public Ingredient(String id, String name, Type type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+}
